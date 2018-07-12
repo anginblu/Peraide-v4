05e2c00f5f24ab87fb339e688e96d531041ffe2d
Jacqueline <anginblu@gmail.com>
2018-07-12 17:11:57 -0400

    fix sessions controller to authenticate

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render json: { :user }
+      render json: user
     else
-      render json: { error: "Incorrect Username or Password."}
+      render json: { error: "Incorrect Username or Password."}, status: 500
     end
   end
 
