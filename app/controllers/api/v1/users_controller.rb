c715477c56db43b5937fabaf889144d4c2f25a37
Jacqueline <anginblu@gmail.com>
2018-07-12 17:08:08 -0400

    add create to userscontroller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    render json: {message: "Signup"}
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages }, status: 500
   end
+
+  private
+   def user_params
+     params.require(:user).permit(:username, :email, :password)
+   end
+
 end
