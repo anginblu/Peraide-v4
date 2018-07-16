6e62de7e0876a4141cb2d3f7d575104940413a90
Jacqueline <anginblu@gmail.com>
2018-07-16 01:27:55 -0400

    fix register form

--- Frontend/src/components/RegisterForm.js
+++ Frontend/src/components/RegisterForm.js
@@ -53,25 +53,39 @@ class RegisterForm extends Component {
       <div className='form-inline'>
         <h2>Register</h2>
         <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="email"
-            onChange={e => this.setState({email: e.target.value})}
-          />
-          <input
-            className="form-control"
-            type="password"
-            placeholder="password"
-            onChange={e => this.setState({password: e.target.value})}
-          />
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={(e) => this.handleSubmit(e)}
-          >
-          Sign Up
-          </button>
+
+          <form onSubmit={e => this.handleSubmit(e)}>
+
+              <label>Username:</label>
+              <input
+              name="username"
+              className="form-control"
+              type="text" placeholder="Please choose a username"
+              onChange={e => this.setState({username: e.target.value})}
+              value={this.state.username}
+              /><br/>
+
+              <label>Email:</label>
+              <input
+              name="email"
+              className="form-control"
+              type="text" placeholder="Please enter your email"
+              onChange={e => this.setState({email: e.target.value})}
+              value={this.state.email}
+              /><br/>
+
+              <label>Password:</label>
+              <input
+              name="password"
+              className="form-control"
+              type="password" placeholder="Please choose a password"
+              onChange={e => this.setState({password: e.target.value})}
+              value={this.state.password}
+              /><br/>
+
+              <button type="submit" className="btn custom-btn">Register</button>
+            </form>
+
         </div>
       </div>
     )
